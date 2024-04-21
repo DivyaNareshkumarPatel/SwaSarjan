362e6ea5c44d308a8b2f805db0c3c934c5517d40
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-04-21 13:36:57 +0530

    Initial commit4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import HomeBanner from '../Components/HomeBanner'
 export default function Home() {
   return (
     <div>
       <Navbar/>
+      <HomeBanner/>
     </div>
   )
 }
