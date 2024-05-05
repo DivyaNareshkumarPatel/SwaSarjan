c6d87faa2091def7043ae81a458cd9c74d999a21
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-05-05 14:43:37 +0530

    about page created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
-
+import About from './Pages/About';
 export default function App() {
   return (
     <Router basename="/SwaSarjan">
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
       </Routes>
     </Router>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,52 @@
-import React, { useState } from 'react';
-import { AppBar, Box, Toolbar, Typography, IconButton, styled, Menu, MenuItem } from '@mui/material';
-import { Search as SearchIcon, Menu as MenuIcon, AccountCircle as AccountCircleIcon, } from '@mui/icons-material';
-import LogoImage from '../images/SwaLogoMain.png';
-
-const Image = styled('img')({ height: 45, margin: 'auto', display: 'flex', padding: '5px 0 0' });
-const Navbar1 = styled(AppBar)({ background: 'white', position: "static" });
-const Wrapper = styled(Box)({ display: 'flex', alignItems: 'center', justifyContent: 'flex-end', flex: 1 });
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  styled,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import {
+  Search as SearchIcon,
+  Menu as MenuIcon,
+  AccountCircle as AccountCircleIcon,
+} from "@mui/icons-material";
+import LogoImage from "../images/SwaLogoMain.png";
+import { Link } from "react-router-dom";
+const Image = styled("img")({
+  height: 45,
+  margin: "auto",
+  display: "flex",
+  padding: "5px 0 0",
+});
+const Navbar1 = styled(AppBar)({ background: "white", position: "static" });
+const Wrapper = styled(Box)({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  flex: 1,
+});
 const NavLink = styled(Typography)(({ isActive }) => ({
-  color: isActive ? '#f26522' : 'black',
-  marginLeft: '20px',
-  marginRight: '20px',
-  textDecoration: 'none',
-  cursor: 'pointer',
-  position: 'relative',
-  '&::after': {
+  color: isActive ? "#f26522" : "black",
+  marginLeft: "20px",
+  marginRight: "20px",
+  textDecoration: "none",
+  cursor: "pointer",
+  position: "relative",
+  "&::after": {
     content: '""',
-    position: 'absolute',
+    position: "absolute",
     left: 0,
-    bottom: '-3px',
-    width: '0',
-    borderBottom: '2px solid #f26522',
-    transition: 'width 0.3s ease',
+    bottom: "-3px",
+    width: "0",
+    borderBottom: "2px solid #f26522",
+    transition: "width 0.3s ease",
   },
-  '&:hover::after': {
-    width: '100%',
+  "&:hover::after": {
+    width: "100%",
   },
 }));
 
@@ -40,48 +63,85 @@ export default function NavBar() {
   };
 
   const menuItems = [
-    { text: 'Home', onClick: () => setActiveMenu('Home') },
-    { text: 'About', onClick: () => setActiveMenu('About') },
+    { text: "Home", to: "/", onClick: () => setActiveMenu("Home") },
+    { text: "About", to: "/about", onClick: () => setActiveMenu("About") },
     // { text: 'Campaign', onClick: () => setActiveMenu('Campaign') ,
     // { text: 'Event', onClick: () => setActiveMenu('Event') },
-    { text: 'Event' , color : '#111' },
-    { text: 'Contact', onClick: () => setActiveMenu('Contact') },
-    { text: 'Registration', onClick: () => setActiveMenu('Registration') },
+    { text: "Event", to: "/event", onClick: () => setActiveMenu("About") },
+    {
+      text: "Contact",
+      to: "/contact",
+      onClick: () => setActiveMenu("Contact"),
+    },
+    {
+      text: "Registration",
+      to: "/registration",
+      onClick: () => setActiveMenu("Registration"),
+    },
   ];
 
   return (
     <Box>
       <Navbar1>
         <Toolbar>
-          <Box><Image src={LogoImage} alt="Logo" /></Box>
-          <Wrapper sx={{ display: { xs: 'flex', md: 'none' } }}>
-            <IconButton color="black" onClick={handleMenuClick}><MenuIcon /></IconButton>
+          <Box>
+            <Image src={LogoImage} alt="Logo" />
+          </Box>
+          <Wrapper sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton color="black" onClick={handleMenuClick}>
+              <MenuIcon />
+            </IconButton>
             <Menu
               anchorEl={anchorEl}
               open={Boolean(anchorEl)}
               onClose={handleMenuClose}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
             >
               {menuItems.map((item, index) => (
-                <MenuItem key={index} onClick={() => { handleMenuClose(); item.onClick(); }}>{item.text}</MenuItem>
+                <MenuItem
+                  key={index}
+                  onClick={() => {
+                    handleMenuClose();
+                    item.onClick();
+                  }}
+                >
+                  {item.text}
+                </MenuItem>
               ))}
             </Menu>
           </Wrapper>
-          <Wrapper sx={{ display: { xs: 'none', md: 'flex' } }}>
+          <Wrapper sx={{ display: { xs: "none", md: "flex" } }}>
             {menuItems.map((item, index) => (
-              <NavLink key={index} fontFamily={''} variant="h7" isActive={activeMenu === item.text} onClick={item.onClick}>{item.text}</NavLink>
+              <NavLink
+                key={index}
+                fontFamily={""}
+                variant="h7"
+                isActive={activeMenu === item.text}
+                onClick={item.onClick}
+              >
+                <Link
+                  to={item.to}
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  {item.text}
+                </Link>
+              </NavLink>
             ))}
           </Wrapper>
           <Box>
-            <IconButton color="#f26522" paddingLeft="5px"><SearchIcon /></IconButton>
-            <IconButton color="dark"><AccountCircleIcon /></IconButton>
+            <IconButton color="#f26522" paddingLeft="5px">
+              <SearchIcon />
+            </IconButton>
+            <IconButton color="dark">
+              <AccountCircleIcon />
+            </IconButton>
           </Box>
         </Toolbar>
       </Navbar1>
