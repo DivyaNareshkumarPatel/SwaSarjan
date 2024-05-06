dfd87275d67773d7c42bcfb982ba6a7c4141612f
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-05-06 12:09:41 +0530

    about section added to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../Components/Navbar";
 import CommonBanner from "../Components/CommonBanner";
+import AboutSwaSarjan from "../Components/AboutSwaSarjan";
 export default function About() {
   return (
     <div>
       <NavBar />
       <CommonBanner head="About Us" line1="Know About us that" line2="Who we are & What we do" />
+      <AboutSwaSarjan/>
     </div>
   );
 }
