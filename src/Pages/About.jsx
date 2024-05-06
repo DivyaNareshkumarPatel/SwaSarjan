2576246876c34aa6936e691b783f6470aa59ad6c
DivyaNareshkumarPatel <divyanareshkumarpatel@gmail.com>
2024-05-06 10:47:28 +0530

    banner added to about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import NavBar from "../Components/Navbar";
+import CommonBanner from "../Components/CommonBanner";
 export default function About() {
   return (
     <div>
       <NavBar />
+      <CommonBanner head="About Us" line1="Know About us that" line2="Who we are & What we do" />
     </div>
   );
 }
