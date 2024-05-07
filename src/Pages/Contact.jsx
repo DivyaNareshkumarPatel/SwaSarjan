590759667e308f1103c0566e32cc3fbe472277c2
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2024-05-07 03:22:48 -0700

    navbar,footer

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 export default function Contact() {
   return (
     <div>
-      
+      <Navbar/>
+      <Footer/>
     </div>
   )
 }
