17945aa2babe452b1408ccdf3361b602748137a3
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2024-05-13 00:28:05 -0700

    gallery video carousel

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -3,15 +3,15 @@ import NavBar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import CommonBanner from '../Components/CommonBanner';
 import GalleryImageGridList from '../Components/GalleryImageGridList';
-// import GalleryVideoSection from '../Components/GalleryVideoSection';
-import GalleryVideoPlayer from '../Components/GalleryVideoPlayer';
+import GalleryVideoCarousel from '../Components/GalleryVideoCarousel';
+
 export default function Gallery() {
     return(
         <div>
             <NavBar/>
             <CommonBanner head="Our Gallery" line1="Know Our Work " line2="Through Documentry" />
             <GalleryImageGridList/>
-            <GalleryVideoPlayer/>
+            <GalleryVideoCarousel/>
             <Footer/>
         </div>
     )
