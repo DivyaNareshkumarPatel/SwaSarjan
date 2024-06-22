ca557dc26c1e639fbcdccb202ab4601628340c88
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2024-06-21 23:18:51 -0700

    password bcrypt done

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,10 +1,28 @@
 import User from "../models/user.js";
-
+import bcrypt from 'bcrypt';
 
 
 export const signupUser = async (request , response) => {
     try{
-        const user = request.body;
+        const hashedPassword = await bcrypt.hash(request.body.password, 10)
+
+        const user = {
+            name: request.body.name,
+            userName: request.body.userName,
+            email: request.body.email,
+            password: hashedPassword,
+            phone: request.body.phone,
+            otp: request.body.otp,
+            dob: request.body.dob,
+            addr: request.body.addr,
+            state: request.body.state,
+            city: request.body.city,
+            pinCode: request.body.pinCode,
+            panCard: request.body.panCard,
+            adharCard: request.body.adharCard,
+            gender: request.body.gender
+        };
+        
         const newUser = new User(user);
         await newUser.save();
         return response.status(200).json({ msg : `signup successfully`})
