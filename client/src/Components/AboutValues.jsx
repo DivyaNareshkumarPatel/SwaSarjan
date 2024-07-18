38dd245ae5d2e3070ad07457aa4d26fcf4e90ffb
krishjp <90134318+KrishJPrajapati@users.noreply.github.com>
2024-07-18 08:22:01 -0700

    update aboutvalues icon size and alignment for smaller devices

--- client/src/Components/AboutValues.jsx
+++ client/src/Components/AboutValues.jsx
@@ -1,15 +1,49 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { styled, Typography, Box } from "@mui/material";
 import values from "../images/Values.png";
 import courage from "../images/courage.png";
 import innovation from "../images/innovation.png";
 import sustain from "../images/sustainability.png";
 import exce from "../images/excellence.png";
+
 export default function AboutValues() {
+
+  const ContentImage = styled(`img`)(({ theme }) => ({
+    width: "150px",
+    marginTop: "20px",
+    marginBottom: "20px",
+    height: "150px",
+    [theme.breakpoints.down("sm")]: {
+      height: "100px",
+    },
+  }));
+
+  const ContentBox = styled(Box)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-evenly",
+    width: "100%",
+    margin: "40px",
+    flexWrap: "wrap",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+  }));
+
+  const ContentItem = styled(Box)(({ theme }) => ({
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: "20px",
+    width: "200px",
+    [theme.breakpoints.down("sm")]: {
+      width: "45%",
+      marginBottom: "20px",
+    },
+  }));
+
   return (
-    <div
-      style={{ backgroundColor: "#0C0C0C", color: "white", padding: "40px" }}
-    >
+    <div style={{ backgroundColor: "#0C0C0C", color: "white", padding: "40px" }}>
       <div
         style={{
           display: "flex",
@@ -33,48 +67,41 @@ export default function AboutValues() {
             SwaSarjan Values
           </Typography>
         </div>
-        <div style={{display:"flex", justifyContent:"space-evenly", width:"100%", margin:"40px", flexWrap:"wrap"}}>
-          <div>
-            <div
-              style={{ height: "2px", width: "200px", backgroundColor: "gray" }}
-            ></div>
+
+        <ContentBox>
+          <ContentItem>
+            <div style={{ height: "2px", width: "200px", backgroundColor: "gray" }}></div>
             <div>
-              <img src={courage} alt="" style={{width:"150px", marginTop:"20px", marginBottom:"20px", height:"150px"}}/>
+              <ContentImage src={courage} alt="" />
               <br />
-              <Typography style={{fontSize:"20px", fontWeight:"bold"}}>Courage</Typography>
+              <Typography style={{ fontSize: "20px", fontWeight: "bold" }}>Courage</Typography>
             </div>
-          </div>
-          <div>
-            <div
-              style={{ height: "2px", width: "200px", backgroundColor: "gray" }}
-            ></div>
+          </ContentItem>
+          <ContentItem>
+            <div style={{ height: "2px", width: "200px", backgroundColor: "gray" }}></div>
             <div>
-              <img src={innovation} alt="" style={{width:"150px", marginTop:"20px", marginBottom:"20px", height:"150px"}}/>
+              <ContentImage src={innovation} alt="" />
               <br />
-              <Typography style={{fontSize:"20px", fontWeight:"bold"}}>Innovation</Typography>
+              <Typography style={{ fontSize: "20px", fontWeight: "bold" }}>Innovation</Typography>
             </div>
-          </div>
-          <div>
-            <div
-              style={{ height: "2px", width: "200px", backgroundColor: "gray" }}
-            ></div>
+          </ContentItem>
+          <ContentItem>
+            <div style={{ height: "2px", width: "200px", backgroundColor: "gray" }}></div>
             <div>
-              <img src={sustain} alt="" style={{width:"150px", marginTop:"20px", marginBottom:"20px", height:"150px"}}/>
+              <ContentImage src={sustain} alt="" />
               <br />
-              <Typography style={{fontSize:"20px", fontWeight:"bold"}}>Sustainability</Typography>
+              <Typography style={{ fontSize: "20px", fontWeight: "bold" }}>Sustainability</Typography>
             </div>
-          </div>
-          <div>
-            <div
-              style={{ height: "2px", width: "200px", backgroundColor: "gray" }}
-            ></div>
+          </ContentItem>
+          <ContentItem>
+            <div style={{ height: "2px", width: "200px", backgroundColor: "gray" }}></div>
             <div>
-              <img src={exce} alt="" style={{width:"150px", marginTop:"20px", marginBottom:"20px", height:"150px"}} />
+              <ContentImage src={exce} alt="" />
               <br />
-              <Typography style={{fontSize:"20px", fontWeight:"bold"}}>Excellence</Typography>
+              <Typography style={{ fontSize: "20px", fontWeight: "bold" }}>Excellence</Typography>
             </div>
-          </div>
-        </div>
+          </ContentItem>
+        </ContentBox>
       </div>
     </div>
   );
